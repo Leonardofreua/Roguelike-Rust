74ddf8a398e63f29e9d511bb616756d6bc8becfb
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-08-05 21:57:09 -0300

    Implemented random movements.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,11 @@ struct Renderable {
     bg: RGB,
 }
 
+#[derive(Component)]
+struct LeftMover {}
+
+struct LeftWalker {}
+
 struct State {
     ecs: World
 }
@@ -23,11 +28,36 @@ struct State {
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
+        
+        self.run_systems();
+        
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
-
+        
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut left_walker = LeftWalker{};
+        left_walker.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (
+        ReadStorage<'a, LeftMover>,
+        WriteStorage<'a, Position>
+    );
+
+    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79; }
         }
     }
 }
@@ -45,6 +75,7 @@ fn main() -> rltk::BError {
     // Registering the components
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
 
     // Creating an Entity
     gs.ecs
@@ -66,6 +97,7 @@ fn main() -> rltk::BError {
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
+            .with(LeftMover{})
             .build();
     }
 
