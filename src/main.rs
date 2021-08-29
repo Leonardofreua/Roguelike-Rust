8740958990ba1ab53af6e62efcca8506088dfdf5
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-08-28 23:44:55 -0300

    Add Monsters.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use specs::prelude::*;
-use rltk::{GameState, Rltk, RGB};
+use rltk::{GameState, Rltk, RGB, Point};
 
 mod components;
 mod constants;
@@ -7,22 +7,32 @@ mod player;
 mod map;
 mod rect;
 mod visibility_system;
+mod monster_ai_system;
 
 pub use constants::{COORDINATE_79, VISIBLE_TILES_RANGE};
-pub use components::{Position, Renderable, LeftMover, Player, Viewshed};
+pub use components::{Position, Renderable, LeftMover, Player, Viewshed, Monster, Name};
 pub use map::{Map, TileType, draw_map};
 pub use rect::Rect;
 use player::player_input;
 use visibility_system::VisibilitySystem;
+use monster_ai_system::MonsterAI;
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum RunState { Paused, Running }
 
 pub struct State {
-    ecs: World
+    pub ecs: World,
+    pub runstate: RunState,
 }
 
 impl State {
     fn run_systems(&mut self) {
         let mut visibility = VisibilitySystem{};
         visibility.run_now(&self.ecs);
+
+        let mut mob = MonsterAI{};
+        mob.run_now(&self.ecs);
+
         self.ecs.maintain();
     }
 }
@@ -30,17 +40,25 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
-        
-        player_input(self, ctx);
-        self.run_systems();
+
+        if self.runstate == RunState::Running {
+            self.run_systems();
+            self.runstate = RunState::Paused;
+        } else {
+            self.runstate = player_input(self, ctx);
+        }
 
         draw_map(&self.ecs, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
-        
+        let map = self.ecs.fetch::<Map>();
+
         for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            let index = map.get_index_xy(pos.x, pos.y);
+            if map.visible_tiles[index] {
+                ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            }
         }
     }
 }
@@ -52,7 +70,8 @@ fn main() -> rltk::BError {
         .with_title("Roguelike Game")
         .build()?;
     let mut gs = State {
-        ecs: World::new()
+        ecs: World::new(),
+        runstate: RunState::Running,
     };
 
     // Registering the components
@@ -61,11 +80,41 @@ fn main() -> rltk::BError {
     gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
     gs.ecs.register::<Viewshed>();
-    
+    gs.ecs.register::<Monster>();
+    gs.ecs.register::<Name>();
+
     // Set a new map
     let map: Map = Map::new_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+    for (i, room) in map.rooms.iter().skip(1).enumerate() {
+        let (x, y) = room.center();
+
+        let glyph: rltk::FontCharType;
+        let name: String;
+        let roll = rng.roll_dice(1, 2);
+
+        match roll {
+            1 => { glyph = rltk::to_cp437('g'); name = "Goblin".to_owned(); }
+            _ => { glyph = rltk::to_cp437('o'); name = "Orc".to_owned(); }
+        }
+
+        gs.ecs.create_entity()
+            .with(Position { x, y })
+            .with(Renderable{
+                glyph: glyph,
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(Viewshed{ visible_tiles: Vec::new(), range: VISIBLE_TILES_RANGE, dirty: true })
+            .with(Monster{})
+            .with(Name{ name: format!("{} #{}", &name, i) })
+            .build();
+    }
+
     gs.ecs.insert(map);
+    gs.ecs.insert(Point::new(player_x, player_y));
 
     // Creating Player entity
     gs.ecs
@@ -78,6 +127,7 @@ fn main() -> rltk::BError {
         })
         .with(Player{})
         .with(Viewshed{ visible_tiles: Vec::new(), range: VISIBLE_TILES_RANGE, dirty: true })
+        .with(Name{ name: "Player".to_owned() })
         .build();
 
     rltk::main_loop(context, gs)
