8a9bd2d310601e67bcba9cca24703fd9657ae93c
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-09-01 17:30:29 -0300

    Logging attacks

--- src/melee_combat_system.rs
+++ src/melee_combat_system.rs
@@ -1,18 +1,25 @@
 use specs::prelude::*;
-use rltk::console;
-use super::{CombatStats, WantsToMelee, Name, SufferDamage};
+use super::{CombatStats, WantsToMelee, Name, SufferDamage, GameLog};
 
 pub struct MeleeCombatSystem {}
 
 impl<'a> System<'a> for MeleeCombatSystem {
+  #[allow(clippy::type_complexity)]
   type SystemData = (Entities<'a>,
                      ReadStorage<'a, Name>,
                      ReadStorage<'a, CombatStats>,
                      WriteStorage<'a, SufferDamage>,
-                     WriteStorage<'a, WantsToMelee>);
+                     WriteStorage<'a, WantsToMelee>,
+                     WriteExpect<'a, GameLog>);
 
   fn run(&mut self, data: Self::SystemData) {
-    let (entities, names, combat_stats, mut inflict_damage, mut wants_melee) = data;
+    let (
+      entities, 
+      names, 
+      combat_stats, 
+      mut inflict_damage, 
+      mut wants_melee,
+      mut log) = data;
 
     for (_entity, name, stats, wants_melee) in (&entities, &names, &combat_stats, &wants_melee).join() {
       if stats.hp > 0 {
@@ -23,9 +30,9 @@ impl<'a> System<'a> for MeleeCombatSystem {
           let damage = i32::max(0, stats.power - target_stats.defense);
 
           if damage == 0 {
-            console::log(&format!("{} is unable to hurt {}", &name.name, &target_name.name));
+            log.entries.push(format!("{} is unable to hurt {}", &name.name, &target_name.name));
           } else {
-            console::log(&format!("{} hits {}, for {} hp", &name.name, &target_name.name, damage));
+            log.entries.push(format!("{} hits {}, for {} hp", &name.name, &target_name.name, damage));
             SufferDamage::new_damage(&mut inflict_damage, wants_melee.target, damage);
           }
         }
